0ff1e34f442f7e586502a25ce6f5dc3f136ac224
Dinh_Nhan <nhan01282544104@gmail.com>
2025-11-28 00:22:07 +0700

    Init Code.

---
+++ backend/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Enable Swagger in Development
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "API đang chạy!");
+
+app.Run();
